89614036e893e2c2771a46ae5e1df08f62642d5b
sebastian-VB <sebas99peru@gmail.com>
2023-07-11 12:26:13 -0500

    project start

---
+++ src/main/java/com/sebdev/apirestauthorbooksrelations/ApiRestAuthorBooksRelationsApplication.java
@@ -0,0 +1,13 @@
+package com.sebdev.apirestauthorbooksrelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestAuthorBooksRelationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestAuthorBooksRelationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebdev/apirestauthorbooksrelations/ApiRestAuthorBooksRelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebdev.apirestauthorbooksrelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestAuthorBooksRelationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
